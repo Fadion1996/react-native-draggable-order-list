ae47d19cd450aff761dd5190e74f5e5ea66ba900
Vasyl Yurkovets <yurkovec.v@gmail.com>
2025-12-05 16:58:01 +0200

    update dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import DraggableList, { DraggableListProps } from './components/DraggableList';
 import { DraggableListRef, TItem } from './types';
 
 export type { DraggableListProps } from './components/DraggableList';
-export type { TItem } from './types';
+export type { TItem, DraggableListRef } from './types';
 
 export default DraggableList as <T extends TItem>(
   props: DraggableListProps<T> & { ref?: React.Ref<DraggableListRef<T>> }
